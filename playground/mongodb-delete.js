8ac4b9f24925360c206d31f1dbb15e45154641e1
anirban <debanirban89@gmail.com>
2018-05-26 20:19:08 +0530

    Added mongo-delete functionality

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb');  // Destructuring Library.. Below is an example
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client)=>{
+    if(err){
+        return console.log('Unable to connect to DB');
+    }
+    console.log('Connected to mongo server');
+
+    var db = client.db('TodoApp');
+
+   //Delete Many
+    // db.collection('todos').deleteMany({text:'I need to dance'}).then((result)=>{
+    //    console.log(result);   
+    // },(e)=>{
+    //     console.log('Error',e);
+    // });
+
+    // db.collection('todos').deleteOne({text:'I need to shop'}).then((result)=>{
+    //     console.log(result);   
+    //  },(e)=>{
+    //      console.log('Error',e);
+    //  });
+
+     //findOneAndDelete
+    //  db.collection('todos').findOneAndDelete({completed:false}).then((doc)=>{
+    //     console.log(doc);   
+    //  },(e)=>{
+    //      console.log('Error',e);
+    //  });
+
+
+    
+
+});
