f45d75f0fd0ee6d3b42fd0a70eae2a7c7bbd0216
anirban <debanirban89@gmail.com>
2018-05-29 01:56:55 +0530

    Added MongDB config chages for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
+var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var _ = require('lodash');
 
